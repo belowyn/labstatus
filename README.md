7bca204396065a8ebac72b4a7833d464cefe42a5
Øystein Røysland Sørlie <belowyn@gmail.com>
2013-10-03 18:42:44 +0200

    some cleanup

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ labstatus
 =========
 
 Script used in checking status for computer labs
+
+Takes a file with host-names to check as input.
--- labstatus.py
+++ labstatus.py
@@ -5,24 +5,12 @@ import sys, os, subprocess
 
 usage = "usage: " + str(sys.argv[0]) + " <labmap> <lablist>"
 
-class bcolors:
-    green = '\033[92m'
-    red = '\033[91m'
-    endC = '\033[0m'
-
-    def disable(self):
-        self.green = ''
-        self.red = ''
-        self.endC = ''
-
 def readfile(inmap, inlist):
     map = str(inmap.read())
     iplist = []
     for line in inlist:
         iplist.append(line.replace('\n', ''))
-    #fping(iplist)
     leMagic(map, iplist)
-    #print map.format(*iplist)
 
 def fping(iplist):
     
@@ -35,17 +23,10 @@ def fping(iplist):
         sub.stdin.write('%s\n' % ip)
     sub.stdin.close()
 
-        #for ip, res in sub.stdout:
-        #    if 'is alive' in res:
-        #        print bcolors.green + ip + bcolors.endC
-        #    elif 'is unreachable' in res:
-        #        print bcolors.red + ip + bcolors.endC
     for line in sub.stdout:
         if 'is alive' in line:
-            #print bcolors.green + line + bcolors.endC
             up.append(line.split(' ')[0])
         elif 'is unreachable' in line:
-            #print bcolors.red + line + bcolors.endC
             down.append(line.split(' ')[0])
             
     return up, down
@@ -53,10 +34,6 @@ def fping(iplist):
 def leMagic(map, iplist):
 
     up, down = fping(iplist)
-
-
-    #print up
-    #print down
     col = []
 
     for ip in iplist:
@@ -65,9 +42,6 @@ def leMagic(map, iplist):
         if ip in down:
             col.append('\033[91m' + ip + '\033[0m')
 
-    #for bajs in col:
-    #    print bajs
-    #print iplist
     print map.format(*col)
 
 if __name__ == "__main__":
